0c78bb7d5a65f34cf87e579e7d83d5ccbff5c0b3
ahmedhegazy <ahmed.hegazy@safavisa.com>
2021-09-24 18:35:43 +0200

    init maven dependencies

---
+++ src/test/java/com/example/springfirebasecrud/SpringFirebaseCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springfirebasecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFirebaseCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
